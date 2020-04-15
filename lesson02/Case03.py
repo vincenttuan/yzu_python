a6f4cc1f95c05c0f39fdc43c199642169a119c9c
LE-T <vincentjava@yahoo.com.tw>
2020-04-15 19:49:50 +0800

    Python

--- lesson02/Case03.py
+++ lesson02/Case03.py
@@ -5,3 +5,10 @@ print(r)
 
 r = random.randint(0, 2)  # 0 <= r <= 2
 print(r)
+
+# 電腦選號四星彩
+n1 = random.randint(0, 9)
+n2 = random.randint(0, 9)
+n3 = random.randint(0, 9)
+n4 = random.randint(0, 9)
+print(n1, n2, n3, n4)
