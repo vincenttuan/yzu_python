2e75f4b5365ec3fea06a443bbe147523d59b4c88
LE-T <vincentjava@yahoo.com.tw>
2020-04-15 19:12:16 +0800

    Python

--- lesson02/Case01.py
+++ lesson02/Case01.py
@@ -7,4 +7,16 @@ if 2 < 0 :
 else:
     print("C")
     print("D")
+
 print("E")
+
+a = 12
+b = 0b101  # 二進位
+c = 0o12   # 八進位
+d = 0x12   # 十六進位
+print(a, b, c, d)
+
+# 型態
+a = 10
+b = 3.14
+print(type(a), type(b))
