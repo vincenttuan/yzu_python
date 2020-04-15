0727156f862da6f999f8a2851818241421bb8d0f
LE-T <vincentjava@yahoo.com.tw>
2020-04-15 20:52:05 +0800

    Python

--- lesson02/Case05.py
+++ lesson02/Case05.py
@@ -6,4 +6,5 @@ shares = 5000
 
 print("2330.TW 台積電成交價 286.5 買進 5000 股 (5張) 成本 1,432,500.0")
 # %s(放字串) %d(放整數) %f(放浮點數)
-print("%s %s成交價 %.1f 買進 %d 股 (%d張) 成本 %.1f")
+print("%s %s成交價 %.1f 買進 %d 股 (%d張) 成本 %.1f" %
+      (symbol, name, price, shares, shares/1000, price * shares))
