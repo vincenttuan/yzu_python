ec8d6855c2f55391798d565051a4f8c107c59d59
LE-T <vincentjava@yahoo.com.tw>
2020-04-15 21:20:10 +0800

    Python

--- lesson02/Case09.py
+++ lesson02/Case09.py
@@ -3,5 +3,6 @@
 h = float(input('請輸入身高:'))
 w = float(input('請輸入體重:'))
 bmi = w / (h/100)**2
-print('身高: %.1f 體重: %.1f BMI: %.2f' %
-      (h, w, bmi))
+result = 18 <= bmi < 23
+print('身高: %.1f 體重: %.1f BMI: %.2f Result: %s' %
+      (h, w, bmi, result))
