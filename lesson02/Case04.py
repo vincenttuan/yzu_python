d92e7494c239dd3b7909c5a78ca112b9ab05b6c5
LE-T <vincentjava@yahoo.com.tw>
2020-04-15 20:29:24 +0800

    Python

--- lesson02/Case04.py
+++ lesson02/Case04.py
@@ -1,1 +1,10 @@
 # -*- coding:UTF-8 -*-
+score = 75
+print("score", score)
+print("score", score, sep="=")
+print("score", score, sep=",")
+print("score", score, sep="&")
+print("a")
+print("b")
+print("a", end="")
+print("b")
