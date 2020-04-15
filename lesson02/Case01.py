2c4a9acbf2cfe74ad52a743c60030b24d292f230
LE-T <vincentjava@yahoo.com.tw>
2020-04-15 19:57:28 +0800

    Python

--- lesson02/Case01.py
+++ lesson02/Case01.py
@@ -3,7 +3,7 @@ import keyword
 
 print("我是中文")
 
-if 2 < 0 :
+if 2 < 0:
     print("A")
     print("B")
 else:
@@ -14,14 +14,14 @@ print("E")
 
 a = 12
 b = 0b101  # 二進位
-c = 0o12   # 八進位
-d = 0x12   # 十六進位
+c = 0o12  # 八進位
+d = 0x12  # 十六進位
 print(a, b, c, d)
 
 # 型態
 a = 10
 b = 3.14
-c = 1+2j
+c = 1 + 2j
 print(type(a), type(b), c, type(c))
 
 # 變數初始值
--- lesson02/Case03.py
+++ lesson02/Case03.py
@@ -12,3 +12,6 @@ n2 = random.randint(0, 9)
 n3 = random.randint(0, 9)
 n4 = random.randint(0, 9)
 print(n1, n2, n3, n4)
+
+# 跳脫字元
+print("\"")
