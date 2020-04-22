66b4c823f18ff4a1b20c1f331f216a6435668398
LE-T <vincentjava@yahoo.com.tw>
2020-04-22 21:01:40 +0800

    Python

--- lesson03/GuessNumber.py
+++ lesson03/GuessNumber.py
@@ -5,6 +5,12 @@ max = 100
 
 while True:
     guess = int(input('請輸入數字 %d ~ %d :' % (min, max)))
+    # 先驗證數字是否在合法範圍 ?
+    if guess <= min or guess >= max:
+        print('數字範圍錯誤, 請重新輸入 !')
+        continue
+
+    # 進行遊戲比對
     if guess == ans:
         print('恭喜答對了')
         break;
