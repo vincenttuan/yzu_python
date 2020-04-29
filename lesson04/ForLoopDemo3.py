b8dd334814dc1357b9d78b40497ba334dbd0bc1e
LE-T <vincentjava@yahoo.com.tw>
2020-04-29 19:04:58 +0800

    Python

--- lesson04/ForLoopDemo3.py
+++ lesson04/ForLoopDemo3.py
@@ -4,3 +4,14 @@ e3 = {'name': 'Bob', 'salary': 70000}
 emps = [e1, e2, e3]
 print(emps)
 # 求薪資總和 ?
+sum = 0
+for emp in emps:
+    sum = sum + emp['salary']
+print(sum)
+
+salary = []
+for emp in emps:
+    salary.append(emp['salary'])
+print(salary)
+print(max(salary))
+print(min(salary))
