18737b1330ab9dc26c14164266b967ab0ed3f4f0
LE-T <vincentjava@yahoo.com.tw>
2020-05-06 19:24:58 +0800

    Python

--- lesson05/DefDemo5.py
+++ lesson05/DefDemo5.py
@@ -1,11 +1,18 @@
 x = 0  # global var
 y = 0  # global var
+z = [0]
 def changeX(n):
     x = n # local var
-
 def changeY(n):
     global y
     y = n
+
+def changeZ(m, n):
+    m[0] = n
+
+print("z=", z)
+changeZ(z, 100)
+print("z=", z)
 
 print("x=", x)
 changeX(100)
