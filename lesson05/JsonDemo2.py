ec5af8e90b3ae1570f44302ef38a9bd2a8ddc3f0
LE-T <vincentjava@yahoo.com.tw>
2020-05-06 21:10:54 +0800

    Python

--- lesson05/JsonDemo2.py
+++ lesson05/JsonDemo2.py
@@ -2,3 +2,9 @@ import json
 import requests
 
 url = 'https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx'
+r = requests.get(url)
+#print(r.status_code)
+#print(r.text)
+bad_rices = json.loads(r.text)
+for bad in bad_rices:
+    print(bad.get('品名'))
