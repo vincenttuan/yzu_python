35f0c07f004cf8254decdd5c4b59685338449579
LE-T <vincentjava@yahoo.com.tw>
2020-05-13 21:20:05 +0800

    Python

--- lesson06/NewsJson.py
+++ lesson06/NewsJson.py
@@ -9,4 +9,11 @@ for d in data.get('data'):
     dict = {'title':d.get('title'), 'headlines':d.get('headlines')}
     news_list.append(dict)
 
-print(news_list)
+file = open('news3.txt', 'a')
+for news in news_list:
+    for head in news['headlines']:
+        if '譚德塞' in head[1]:
+            print(head)
+            file.writelines(head)
+            file.write("\n")
+
