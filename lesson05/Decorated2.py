749b9892aa750a6a38e2eb9bcd0b172c90a93928
LE-T <vincentjava@yahoo.com.tw>
2020-05-13 19:14:45 +0800

    Python

---
+++ lesson05/Decorated2.py
@@ -0,0 +1,16 @@
+def login(func, password):
+    def check():
+        if password == 1234:
+            print("登入成功")
+            func()
+        else:
+            print("登入失敗")
+            return None
+    return check
+
+def report():
+    print("密件: 今日頭條...")
+
+
+report()
+login(report, 12345)()
