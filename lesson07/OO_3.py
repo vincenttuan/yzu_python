dae63f16e3a454255375a3860f4e0f64b04836af
LE-T <vincentjava@yahoo.com.tw>
2020-05-20 19:22:16 +0800

    Python

--- lesson07/OO_3.py
+++ lesson07/OO_3.py
@@ -2,6 +2,10 @@ class Account:
     name = '' # 公有變數
     __balance = 100  # 私有變數
 
+    def __init__(self, name, money):
+        self.name = name
+        self.__balance = self.__balance + money
+
     def addBalance(self, money):
         if money > 0:
             self.__balance = self.__balance + money
@@ -11,11 +15,7 @@ class Account:
 
 if __name__ == '__main__':
     Account.bank = '元大銀行' # 靜態/類別變數
-    acc = Account()
-    acc.name = 'Vincent'
-    acc.addBalance(200)
+    acc = Account('Vincent', 200)
     print(Account.bank, acc)
-    acc2 = Account()
-    acc2.name = 'Mary'
-    acc2.addBalance(500)
+    acc2 = Account('Mary', 500)
     print(Account.bank, acc2)
