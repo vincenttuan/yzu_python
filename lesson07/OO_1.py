64de91e60e98f57ea8f921bfdc31340ad3f9dafe
LE-T <vincentjava@yahoo.com.tw>
2020-05-20 19:26:39 +0800

    Python

--- lesson07/OO_1.py
+++ lesson07/OO_1.py
@@ -2,9 +2,15 @@ class Human:
     name = ''
     sex = ''
     age = 0
-
     def __str__(self):  # self 相當於 java 中的 this
         return self.name + ", " + self.sex + ", " + str(self.age)
+
+class Student(Human):
+    number = 0
+    grade = ''
+    def __str__(self):
+        return ", " + str(self.number) + ", " + self.grade
+
 
 #print(__name__)
 if __name__ == '__main__':
@@ -14,3 +20,5 @@ if __name__ == '__main__':
     h.age = 18
     print(h.name, h.sex, h.age)
     print(h)
+
+
