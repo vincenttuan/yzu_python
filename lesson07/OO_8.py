7941100aee3c0a23b9ceca4209efc61eae67e93b
LE-T <vincentjava@yahoo.com.tw>
2020-05-20 20:26:37 +0800

    Python

---
+++ lesson07/OO_8.py
@@ -0,0 +1,18 @@
+class DictDemo:
+    def __init__(self):
+        self.dict = {}
+
+    def __getitem__(self, item):
+        return self.dict[item]
+
+    def __setitem__(self, key, value):
+        self.dict[key] = value
+
+    def __len__(self):
+        return len(self.dict)
+
+d = DictDemo()
+d['h'] = 180
+d['w'] = 60
+
+print(d['h'], d['w'], len(d))
