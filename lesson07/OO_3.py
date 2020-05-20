b9ddfc8d388488d8f0fbe8a06168515b92c1eb6e
LE-T <vincentjava@yahoo.com.tw>
2020-05-20 19:18:29 +0800

    Python

--- lesson07/OO_3.py
+++ lesson07/OO_3.py
@@ -3,13 +3,19 @@ class Account:
     __balance = 100  # 私有變數
 
     def addBalance(self, money):
-        self.__balance = self.__balance + money
+        if money > 0:
+            self.__balance = self.__balance + money
 
     def __str__(self):
         return self.name + "有 $" + str(self.__balance)
 
 if __name__ == '__main__':
+    Account.bank = '元大銀行' # 靜態變數
     acc = Account()
     acc.name = 'Vincent'
     acc.addBalance(200)
-    print(acc)
+    print(Account.bank, acc)
+    acc2 = Account()
+    acc2.name = 'Mary'
+    acc2.addBalance(500)
+    print(Account.bank, acc2)
