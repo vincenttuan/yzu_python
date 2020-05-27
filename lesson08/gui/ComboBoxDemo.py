a404ffc7711096823308a7bbc3e5e53e4459d1bd
LE-T <vincentjava@yahoo.com.tw>
2020-05-27 21:08:55 +0800

    Python

--- lesson08/gui/ComboBoxDemo.py
+++ lesson08/gui/ComboBoxDemo.py
@@ -4,9 +4,15 @@ from tkinter import ttk
 win = tkinter.Tk()
 win.geometry('300x300')
 
-fruits = ['apple', 'banana', 'mango', 'melonwater']
-combo = ttk.Combobox(win, values=fruits, state="readonly") # readonly, disabled
+label = tkinter.Label(win, text='品項 : ')
+#fruits = ['apple', 'banana', 'mango', 'melonwater']
+fruits = {'apple':50, 'banana':60, 'mango':70, 'melonwater':25}
+combo = ttk.Combobox(win, values=list(fruits.keys()), state="readonly") # readonly, disabled
 combo.current(2)
-combo.pack()
+
+# UI 布局
+label.grid(column=0, row=0, padx=10, pady=10)
+combo.grid(column=1, row=0, padx=10, pady=10)
+
 
 win.mainloop()
