4647c4b2c7276bbb11cd6dcdf5a732f8eb8f6a6d
LE-T <vincentjava@yahoo.com.tw>
2020-05-27 19:54:40 +0800

    Python

--- lesson08/gui/GUIDemo2.py
+++ lesson08/gui/GUIDemo2.py
@@ -3,10 +3,16 @@ import tkinter
 win = tkinter.Tk()
 
 win.title('我的小視窗')
-label = tkinter.Label(win, text='Hello')
-label.pack(side=tkinter.LEFT)
+label1 = tkinter.Label(win, text='Hello1')
 label2 = tkinter.Label(win, text='Hello2')
-label2.pack(side=tkinter.RIGHT)
+label3 = tkinter.Label(win, text='Hello3')
+label4 = tkinter.Label(win, text='Hello4')
+label5 = tkinter.Label(win, text='Hello5')
+label1.grid(column=0, row=0)
+label2.grid(column=1, row=0)
+label3.grid(column=1, row=1)
+label4.grid(column=2, row=2)
+label5.grid(column=2, row=0)
 
 win.geometry('300x300')
 
