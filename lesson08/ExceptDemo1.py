01077614332eb2ca0b647fed746fff7dd07d0f4f
LE-T <vincentjava@yahoo.com.tw>
2020-05-27 18:48:19 +0800

    Python

--- lesson08/ExceptDemo1.py
+++ lesson08/ExceptDemo1.py
@@ -1,3 +1,5 @@
+import sys
+import traceback
 x = 10
 y = int(input('請輸入數字:'))
 try:
@@ -6,3 +8,6 @@ try:
 except Exception as e:
     print('有錯誤發生', e)
     print(e.__class__.__name__)
+    cl, exc, tb = sys.exc_info()
+    lastCallStack = traceback.extract_tb(tb)[-1]
+    print(lastCallStack)
