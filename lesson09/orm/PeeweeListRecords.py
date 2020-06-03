de1463a73774761978f2b0dcd475321fa32e28fe
LE-T <vincentjava@yahoo.com.tw>
2020-06-03 21:28:19 +0800

    Python

--- lesson09/orm/PeeweeListRecords.py
+++ lesson09/orm/PeeweeListRecords.py
@@ -4,4 +4,16 @@ from peewee import *
 
 from lesson09.orm.Entity import Person, Pet
 
+for person in Person.select():
+    print(person.name, person.birthday)
 
+print("----------------------")
+
+for person in Person.select():
+    print(person)
+    for pet in person.pets:
+        print("   ", pet)
+
+print("----------------------")
+for pet in Pet.select():
+    print(pet.name, pet.animal_type, pet.owner.name)
