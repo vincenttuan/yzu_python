ee3e2651836e9b73344855c727214188750b5494
LE-T <vincentjava@yahoo.com.tw>
2020-06-03 21:24:51 +0800

    Python

--- lesson09/orm/PeeweeInsertRecord.py
+++ lesson09/orm/PeeweeInsertRecord.py
@@ -1,12 +1,12 @@
 from datetime import date
 from lesson09.orm.PeeweeCreateTable import Person, Pet
 
-uncle_bob = Person(name='Bob', birthday=date(1985, 1, 15))
-uncle_bob.save()
-grandma = Person.create(name='Mary', birthday=date(1995, 3, 1))
+bob = Person(name='Bob', birthday=date(1985, 1, 15))
+bob.save()
+mary = Person.create(name='Mary', birthday=date(1995, 3, 1))
 herb = Person.create(name='Herb', birthday=date(1970, 5, 5))
 
-kitty = Pet.create(owner=uncle_bob, name='Kitty', animal_type='cat')
+kitty = Pet.create(owner=bob, name='Kitty', animal_type='cat')
 fido = Pet.create(owner=herb, name='Fido', animal_type='dog')
 lucky = Pet.create(owner=herb, name='Lucky', animal_type='cat')
 happy = Pet.create(owner=herb, name='Happy', animal_type='cat')
