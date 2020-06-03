1e7f388e2cfbe5a282aa52e83e4de39c418786f2
LE-T <vincentjava@yahoo.com.tw>
2020-06-03 21:10:53 +0800

    Python

--- lesson09/sql2/ReadRecord.py
+++ lesson09/sql2/ReadRecord.py
@@ -5,17 +5,14 @@ import matplotlib.pyplot as plt
 conn = sqlite3.connect('company.db')
 
 df = pd.read_sql_query("SELECT * FROM Employee", con=conn)
-print(df.shape)
-print(df.dtypes)
-print(df.head())
 print(df)
 
 # 繪圖
 ma = df['SALARY'].rolling(window=2).mean()
-print(ma)
-plt.plot(df.NAME.values, df.SALARY.values, 'r.')
+#print(ma)
+plt.plot(df.NAME.values, df.SALARY.values, 'r.')  # 畫紅點
+plt.plot(df['NAME'], df['SALARY'])  # 畫折線
 plt.plot(df.NAME.values, ma)
-plt.plot(df['NAME'], df['SALARY'])
 plt.xlabel('NAME')
 plt.ylabel('SALARY')
 plt.show()
