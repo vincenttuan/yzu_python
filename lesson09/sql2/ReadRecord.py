ff860912fae839a141040877105afc399e634794
LE-T <vincentjava@yahoo.com.tw>
2020-06-03 21:04:29 +0800

    Python

---
+++ lesson09/sql2/ReadRecord.py
@@ -0,0 +1,24 @@
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+conn = sqlite3.connect('company.db')
+c = conn.cursor()
+
+df = pd.read_sql_query("SELECT * FROM Employee", con=conn)
+print(df.shape)
+print(df.dtypes)
+print(df.head())
+print(df)
+
+# 繪圖
+ma = df['SALARY'].rolling(window=2).mean()
+print(ma)
+plt.plot(df.NAME.values, df.SALARY.values, 'r.')
+plt.plot(df.NAME.values, ma)
+plt.plot(df['NAME'], df['SALARY'])
+plt.xlabel('NAME')
+plt.ylabel('SALARY')
+plt.show()
+
+conn.close()
