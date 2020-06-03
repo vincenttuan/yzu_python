3a5d8a166e036f9488b6a381847a4eb510d02bbd
LE-T <vincentjava@yahoo.com.tw>
2020-06-03 19:50:43 +0800

    Python

--- lesson09/sql/ReadRecords2.py
+++ lesson09/sql/ReadRecords2.py
@@ -4,6 +4,6 @@ import pandas as pd
 sql = "SELECT * FROM Lotto"
 conn = sqlite3.connect("demo.db")
 df = pd.read_sql_query(sql, con=conn)
-print(df.head())
+#print(df.head())
 print(df)
 conn.close()
