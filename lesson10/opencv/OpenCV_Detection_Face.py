c6fd92cf011f22eb7db9ca21a5884657da85b0eb
LE-T <vincentjava@yahoo.com.tw>
2020-06-10 19:33:50 +0800

    Python

--- lesson10/opencv/OpenCV_Detection_Face.py
+++ lesson10/opencv/OpenCV_Detection_Face.py
@@ -42,6 +42,8 @@ while True:
     # 在臉部周圍畫矩形框
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 5)  # 注意：(0, 255, 0) 是 BGR
+        # 繪文字
+        cv2.putText(frame, 'Vincent', (x, y - 7), 16, 1.2, (0, 255, 0), 2)
 
     # 將 frame 顯示
     cv2.imshow('Detect face', frame)
