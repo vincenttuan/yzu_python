56067091488a5a77a11970b00640e60595d0ea24
LE-T <vincentjava@yahoo.com.tw>
2020-06-10 21:22:41 +0800

    Python

--- lesson10/TWII.py
+++ lesson10/TWII.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date=20200609&selectType=ALL'
+url = 'https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date=20200610&selectType=ALL'
 data = requests.get(url).text
 # print(data)
 # "證券代號","證券名稱","殖利率(%)","股利年度","本益比","股價淨值比","財報年/季",
@@ -9,8 +9,8 @@ data = data.replace('"', '')
 for d in data.split("\r\n"):
     list = d.split(",")
     if len(list) == 8 and list[0] != '證券代號':
-        if float(list[2]) > 7.0 and \
-           float(list[4]) < 10 and \
+        if float(list[2]) > 10.0 and \
+           float(list[4]) < 5 and \
            float(list[5]) < 1:
-            print(list, end=',')
+            print(list)
 
