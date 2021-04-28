ccf29d6a48c3bfce237a0c17557faba083b47785
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-04-28 11:24:14 +0800

    Update Case01.py

--- lesson03/Case01.py
+++ lesson03/Case01.py
@@ -2,6 +2,11 @@
 n = 123 * 99 * 1234 * 1.27
 print(format(n, ','))
 
+# 千分位 + 小數點
+n = 123 * 99 * 1234 * 1.27432
+print(format(float("%.2f" % n), ','))
+print("{:,}".format(float("%.2f" % n)))
+
 # 原始字串
 s = r"C:\temp\nba.txt"
 print(s)
